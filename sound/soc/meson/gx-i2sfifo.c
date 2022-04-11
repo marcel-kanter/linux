4bcc6c467df7939c14c463575a4cf7596aa668bf
Marcel Kanter <marcel.kanter@googlemail.com>
2022-04-11 10:20:23 +0200

    Allocate and deallocate the DMA buffer for I2S FIFO component

--- sound/soc/meson/gx-i2sfifo.c
+++ sound/soc/meson/gx-i2sfifo.c
@@ -2,9 +2,23 @@
 /*
 Copyright (c) 2021 Marcel Kanter <marcel.kanter@googlemail.com>
 */
+#include <linux/dma-mapping.h>
 #include <linux/module.h>
 #include <linux/of_device.h>
+#include <sound/pcm_params.h>
 #include <sound/soc.h>
+
+
+#define GX_I2S_FIFO_BLOCK_SIZE 256
+
+
+static struct snd_pcm_hardware gx_i2sfifo_pcm_hardware = {
+	.period_bytes_min = GX_I2S_FIFO_BLOCK_SIZE,
+	.period_bytes_max = GX_I2S_FIFO_BLOCK_SIZE * 32,
+	.periods_min = 2,
+	.periods_max = 64,
+	.buffer_bytes_max = GX_I2S_FIFO_BLOCK_SIZE * 32 * 64,
+};
 
 
 static int gx_i2sfifo_component_probe(struct snd_soc_component *component)
@@ -34,16 +48,84 @@ static int gx_i2sfifo_component_close(struct snd_soc_component *component, struc
 }
 
 
+static int gx_i2sfifo_pcm_allocate_dma_buffer(struct snd_pcm *pcm, int stream)
+{
+	struct snd_pcm_substream *substream = pcm->streams[stream].substream;
+	struct snd_dma_buffer *buf = &substream->dma_buffer;
+	size_t size = gx_i2sfifo_pcm_hardware.buffer_bytes_max;
+
+	buf->dev.type = SNDRV_DMA_TYPE_DEV;
+	buf->dev.dev = pcm->card->dev;
+	buf->private_data = NULL;
+	buf->area = dma_alloc_coherent(pcm->card->dev, size, &buf->addr, GFP_KERNEL);
+
+	if (!buf->area)
+	{
+		return -ENOMEM;
+	}
+
+	buf->bytes = size;
+
+	return 0;
+}
+
+
 static int gx_i2sfifo_component_pcm_construct(struct snd_soc_component *component, struct snd_soc_pcm_runtime *rtd)
 {
+	int ret;
+	struct snd_card *card;
+
 	dev_dbg(component->dev, "gx_i2sfifo_pcm_construct");
+
+	card = rtd->card->snd_card;
+
+	ret = dma_coerce_mask_and_coherent(card->dev, DMA_BIT_MASK(32));
+	if (ret)
+	{
+		dev_err(card->dev, "Setting DMA mask failed: %d", ret);
+		return ret;
+	}
+
+	if (rtd->pcm->streams[SNDRV_PCM_STREAM_PLAYBACK].substream)
+	{
+		ret = gx_i2sfifo_pcm_allocate_dma_buffer(rtd->pcm, SNDRV_PCM_STREAM_PLAYBACK);
+		if (ret)
+		{
+			dev_err(card->dev, "Preallocating DMA buffer failed: %d", ret);
+			return ret;
+		}
+	}
+
 	return 0;
+}
+
+
+static void gx_i2sfifo_pcm_deallocate_dma_buffer(struct snd_pcm *pcm, int stream)
+{
+	struct snd_pcm_substream *substream;
+	struct snd_dma_buffer *buf;
+
+	substream = pcm->streams[stream].substream;
+	if (!substream)
+	{
+		return;
+	}
+
+	buf = &substream->dma_buffer;
+	if (!buf->area)
+	{
+		return;
+	}
+
+	dma_free_coherent(pcm->card->dev, buf->bytes, buf->area, buf->addr);
+	buf->area = NULL;
 }
 
 
 static void gx_i2sfifo_component_pcm_destruct(struct snd_soc_component *component, struct snd_pcm *pcm)
 {
 	dev_dbg(component->dev, "gx_i2sfifo_pcm_destruct");
+	gx_i2sfifo_pcm_deallocate_dma_buffer(pcm, SNDRV_PCM_STREAM_PLAYBACK);
 }
 
 
