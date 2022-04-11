cb1de59f6c4412ecfc7510c5ae9fb71f9e6bd4b8
Marcel Kanter <marcel.kanter@googlemail.com>
2022-04-11 10:14:43 +0200

    Parse device tree of sound card

--- sound/soc/meson/gx-card.c
+++ sound/soc/meson/gx-card.c
@@ -4,11 +4,108 @@ Copyright (c) 2021 Marcel Kanter <marcel.kanter@googlemail.com>
 */
 #include <linux/module.h>
 #include <linux/of_device.h>
+#include <sound/soc.h>
+
+
+static int gx_card_parse_link(struct snd_soc_card *card, struct device_node *node, struct snd_soc_dai_link *link)
+{
+	return 0;
+}
+
+
+static int gx_card_parse_links(struct snd_soc_card *card)
+{
+	int ret;
+	int num_links;
+	struct device_node *node;
+	struct device_node *child;
+	struct snd_soc_dai_link *link;
+
+	node = card->dev->of_node;
+
+	num_links = of_get_child_count(node);
+	if (num_links == 0)
+	{
+		dev_err(card->dev, "No links defined");
+		return -EINVAL;
+	}
+
+	card->num_links = num_links;
+
+	card->dai_link = devm_kcalloc(card->dev, card->num_links, sizeof(*link), GFP_KERNEL);
+	if (!card->dai_link)
+	{
+		dev_err(card->dev, "Failed to allocate memory for the links");
+		return -ENOMEM;
+	}
+
+	link = card->dai_link;
+	for_each_child_of_node(node, child)
+	{
+		ret = gx_card_parse_link(card, child, link);
+		if (ret)
+		{
+			goto error;
+		}
+
+		link++;
+	}
+
+error:
+	of_node_put(child);
+	return ret;
+}
 
 
 static int gx_card_platform_probe(struct platform_device *pdev)
 {
+	int ret;
+	struct snd_soc_card *card;
+
 	dev_dbg(&pdev->dev, "gx_card_platform_probe");
+
+	card = devm_kzalloc(&pdev->dev, sizeof(*card), GFP_KERNEL);
+	if (!card)
+	{
+		dev_err(&pdev->dev, "Failed to allocate memory for sound card structure");
+		return -ENOMEM;
+	}
+
+	platform_set_drvdata(pdev, card);
+
+	card->owner = THIS_MODULE;
+	card->dev = &pdev->dev;
+
+	ret = snd_soc_of_parse_card_name(card, "model");
+	if (ret)
+	{
+		return ret;
+	}
+
+	if (of_property_read_bool(card->dev->of_node, "audio-widgets"))
+	{
+		ret = snd_soc_of_parse_audio_simple_widgets(card, "audio-widgets");
+		if (ret)
+		{
+			return ret;
+		}
+	}
+
+	if (of_property_read_bool(card->dev->of_node, "audio-routing"))
+	{
+		ret = snd_soc_of_parse_audio_routing(card, "audio-routing");
+		if (ret)
+		{
+			return ret;
+		}
+	}
+
+	ret = gx_card_parse_links(card);
+	if (ret)
+	{
+		return ret;
+	}
+
 	return 0;
 }
 
